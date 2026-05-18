a6f0667f88245195ac62570ed236184d34fb08f9
James Miller <jameswil.miller@gmail.com>
2026-05-18 12:20:17 +1000

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        #need a hashmap for frequency of letters in t
+        #need a hashmap for frequency of letters in my current window
+        
+        #track an int have and need
+        # if added letter matches the count in the need table
+        # then increment have
+        # if they are not equal decrement have
+        # finish when have == need
+        
+        if t == "": return ""
+        # initiliase a hashmap countT that tells us the count of each character in t
+        countT, window = {}, {}
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        # populate count T
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+        
+        have, need = 0, len(countT)
+
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+        
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            
+            while have == need:
+                #update the result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = (r - l + 1)
+                #pop from the left of window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res 
+        return s[l:r+1] if resLen != float("infinity") else ""
+
+
+
+        
+        
