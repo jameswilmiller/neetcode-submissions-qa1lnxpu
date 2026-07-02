919113104ca1cc04ef3694648feba617e8969d40
James Miller <jameswil.miller@gmail.com>
2026-07-02 13:28:28 +1000

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # each step we can take the choice of adding 1 or 2
+        
+        # we can use memoisation ?
+        if n <= 2:
+            return n
+        
+        dp = [0] * (n + 1)
+
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+        
+
+
+
+
+        
